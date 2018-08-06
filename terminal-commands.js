a79d92d01e51d3d8441f003dbe4c94fc730be0b3
DazzledAK <youwatchme22@gmail.com>
2018-08-06 11:06:22 -0500

    Completed Assignment Practice

--- terminal-commands.js
+++ terminal-commands.js
@@ -9,3 +9,20 @@ module.exports.ls = () => {
    console.log(filesToString);
   });
 };
+
+module.exports.touch = () => {
+  fs.writeFile('message.txt', 'Hello Node.js', 'utf8', (err) => {
+    if (err) throw err;
+
+    console.log('success');
+  });
+};
+
+module.exports.mkdir = () => {
+  fs.mkdir('./',function(err){
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Directory created successfully!");
+  });
+};
